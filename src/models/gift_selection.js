4d38eaa4779a97ac03bd16dcde17b0c45cbc728b
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-03-09 16:16:24 +0100

    Fetch gift selection with related gift data

--- src/models/gift_selection.js
+++ src/models/gift_selection.js
@@ -1,13 +1,19 @@
 const bookshelf = require('bookshelf_config');
 
+const Gift = require('src/models/gift');
+
 const GiftSelection = bookshelf.Model.extend({
    tableName: 'gift_selection',
+
+    gift: function() {
+       return this.belongsTo(Gift);
+    },
 
    fetchGiftsBasedOnOptions: function (params) {
        const { age_id, budget_id } = params;
        console.log('fetching from database');
        return this.where({ age_id, budget_id })
-           .fetchAll()
+           .fetchAll({ withRelated: 'gift' })
            .catch(error => {
                throw error;
            });
