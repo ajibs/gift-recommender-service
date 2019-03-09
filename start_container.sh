ed4ae11aa58acac3ca667170442ebc6e4f0e92d1
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-03-09 15:29:33 +0100

    Add MySQL Database

--- start_container.sh
+++ start_container.sh
@@ -13,6 +13,24 @@ else
     echo " ----- Image Available for Use. -----"
 fi
 
+PROJECT_NAME="gift_recommender"
+
+CURRENT_DOCKER_COMPOSE_VERSION=$( docker-compose -v | grep -o '[0-9]*[.][0-9]*[.][0-9]' | sed -e 's/[.]//g' )
+BREAKING_DOCKER_COMPOSE_VERSION=1210
+
+# Since docker compose version: '1.21.0', the network setup automatically adds a single '_' to the network_name_prefix,
+# while version '1.20.0' and below replaces any extra ‘_’ at the end of the network_name_prefix with a single '_'.
+# This assumes that at versions below '1.20.0' e.g. '1.9.0', no ‘_’ is added at the end of the network_name_prefix.
+if [[ ${CURRENT_DOCKER_COMPOSE_VERSION} -lt ${BREAKING_DOCKER_COMPOSE_VERSION} ]]; then
+ PROJECT_NAME="${PROJECT_NAME}_"
+fi
+
+echo " ----- Network name prefix is: ${PROJECT_NAME} -----"
+
+echo " ----- Starting Up Infrastructure Containers -----"
+
+docker-compose -p ${PROJECT_NAME} up -d
+
 echo " ----- Run Application in a Disposable Container -----"
 docker run \
     -i \
@@ -20,6 +38,8 @@ docker run \
     -p 8080 \
     --rm \
     -v ${ROOT}:/src \
+    --env-file=${ROOT}/.env \
+    --network=gift_recommender_main_network \
     ${IMAGE_NAME} \
     bash
 
