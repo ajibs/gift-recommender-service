505c3dd96f8db0e7ea94e11071553564b4768077
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-03-09 15:34:15 +0100

    Setup database Migrations and Seeds

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+const config = require('src/config/config');
+
+const {
+  host,
+  name,
+  user,
+  password,
+  pool
+} = config.database;
+
+const { min, max } = pool;
+
+const dbConfig = {
+  client: 'mysql',
+  connection: {
+    host,
+    user,
+    password,
+    database: name
+  },
+  pool: {
+    min,
+    max,
+  },
+  migrations: {
+    tableName: 'migrations'
+  }
+};
+
+module.exports = {
+  development: dbConfig,
+  production: dbConfig
+};
