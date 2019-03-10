b249a694cf619a31b99e2b3c4cc7df72e774e2d1
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-03-10 17:58:20 +0100

    Allow CORS and parse response body of requests

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const config = require('src/config/config');
 const giftSelectionController = require('src/controllers/gift_selection');
 const logger = require('src/lib/logger');
 const PORT = config.webServer.port;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.get('/gifts', (req, res) => giftSelectionController(req, res));
+app.post('/gifts', (req, res) => giftSelectionController(req, res));
 
 app.listen(PORT, () => {
     logger.info(`App started and listening on port: ${PORT}`);
