ee9d7dec2908c28ad82aa58b8fb9bd15d862a2bf
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-04-04 17:26:01 +0100

    Added mocha and co for testing with sample tests

---
+++ tests/regression/app.js
@@ -0,0 +1,10 @@
+const server = require('server');
+const request = require('supertest')(server);
+
+describe('App Setup', () => {
+    it('should successfully get a response from server', (done) => {
+        request.get('/')
+            .expect(200)
+            .end(done);
+    });
+});
