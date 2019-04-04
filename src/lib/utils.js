f619cccfc1ceb9bc5e5ab6f3e4fb4eb81ee767d5
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-04-04 13:35:26 +0100

    Shuffle all gift ideas when responding to request from client

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,6 +6,15 @@ class Utils {
             .join('_')
             .replace('-', '_');
     }
+
+    static shuffleArray (array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+
+        return array;
+    }
 }
 
 module.exports = Utils;
--- src/services/gift_idea.js
+++ src/services/gift_idea.js
@@ -1,12 +1,14 @@
 const logger = require('src/lib/logger');
 const GiftIdea = require('src/models/gift_idea');
+const Utils = require('src/lib/utils');
+const shuffleArray = Utils.shuffleArray;
 
 const giftIdeaService = (reqId) => new Promise((resolve, reject) => {
-    logger.info(`Request ID : ${reqId} - fetching gifts ideas from service`);
+    logger.info(`Request ID : ${reqId} - fetching all gifts ideas from service`);
 
     new GiftIdea().fetchAllGiftIdeas()
         .then((response) => {
-            resolve(response);
+            resolve(shuffleArray(response));
         })
         .catch(error => {
             reject(error);
